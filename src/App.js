3b460962a0a3c24870f0bacba16371d0e6d0ffe0
ritika-saha <ritika882001@gmail.com>
2023-07-09 03:29:22 +0530

    basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Home from './screen/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Login from './screen/Login';
 
 function App() {
   return (
+    <Router>
     <div>
-        
-    </div>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/login' element={<Login />} />
+      </Routes>
+      </div> 
+    </Router>
   );
 }
 
