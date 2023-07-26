165747d879908c3b19777c1b91bc71b5b620382c
ritika-saha <ritika882001@gmail.com>
2023-07-26 22:09:28 +0530

    fetching collection data

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
-const mongoURI='mongodb+srv://FoodExpress:jkhGUbSTNXQRwyDo@cluster0.cpx0kdu.mongodb.net/?retryWrites=true&w=majority'
-
+const mongoURI='mongodb+srv://FoodExpress:jkhGUbSTNXQRwyDo@cluster0.cpx0kdu.mongodb.net/FoodExpressDB?retryWrites=true&w=majority' 
+//add the db name before ? in the mongo uri-------------important
 const mongoDB = async () => {
-    try {
-      await mongoose.connect(mongoURI);
-      console.log('Connected to the db');
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err);
-    }
-  };
-  
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('Connected!');
+    let fetched_data = mongoose.connection.db.collection("food_items");
+    //toArray returns a promise which can be resolved with a await
+    let data=await fetched_data.find({}).toArray() 
+    console.log(data);
+  } catch (error) {
+    console.log('err: ', error);
+  }
+};
+
+
 
 module.exports= mongoDB;
