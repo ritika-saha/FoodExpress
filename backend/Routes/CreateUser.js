88da42cc9f69f0df63c4e535a597e57b64098def
ritika-saha <ritika882001@gmail.com>
2023-07-30 22:10:10 +0530

    validation for post request on schema

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,8 +1,21 @@
 const express=require('express')
 const router=express.Router()
 const User=require('../models/User')
+const { validationResult,body } = require('express-validator');
 
-router.post("/createuser",async(req,res)=>{
+router.post("/createuser",
+
+[
+    body('email').isEmail(),
+    body('password','password len should be >5').isLength({min: 5})
+]
+,async(req,res)=>{
+    
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
     try{
        await User.create({
             name:req.body.name,
