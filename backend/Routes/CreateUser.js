535213e26ab39258b45e4c03a38fa3a15ae8b95c
ritika-saha <ritika882001@gmail.com>
2023-08-13 22:09:16 +0530

    login for old users

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,8 +1,13 @@
+//SINCE IT IS SMALL PIECE OF CODE THE LOGIN AND SIGN UP ENDPOINT HAVE BEEN MADE ON THIS SAME FILE IT SELF
+//WITHOUT MAKING DIFFERENT FILES FOR EACH
+
+
 const express=require('express')
 const router=express.Router()
 const User=require('../models/User')
 const { validationResult,body } = require('express-validator');
 
+//user creation end point
 router.post("/createuser",
 
 //validation
@@ -25,6 +30,42 @@ router.post("/createuser",
             location:req.body.location
         })
         res.json({success:true})
+    } catch(error){
+        console.log(error)
+        res.json({success:false})
+    }
+})
+
+
+//user login endpoint
+
+router.post("/loginuser",//validation
+[
+    body('email').isEmail(),
+    body('password','password len should be >5').isLength({min: 5})
+],
+
+async(req,res)=>{
+
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+let email=req.body.email
+    try{
+        //returns the entire data that contains this particular email and the data gets stored into userData
+      let userData= await User.findOne({email})
+      if(!userData){
+        return res.status(400).json({errors: "Invalid Credential"})
+      }
+      //if the password in the data containing the given email does not match the user entered password
+      if(req.body.password!==userData.password){
+        return res.status(400).json({errors: "Invalid Credential"})
+      }
+      
+      return res.json({success:true})
+        
     } catch(error){
         console.log(error)
         res.json({success:false})
