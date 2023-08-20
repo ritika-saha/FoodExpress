061acf4a1039c4ad18be3ecfec9a6cdd0fe3450a
ritika-saha <ritika882001@gmail.com>
2023-08-20 20:54:04 +0530

    food data categorization and fetching

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import Card from "../components/Card";
 import Carousal from "../components/Carousal";
 
 const Home = () => {
+
+  const [foodCat,setFoodCat]=useState([])
+  const [foodItem,setFoodItem]=useState([])
+
+  const loadData= async ()=>{
+    let response=await fetch("http://localhost:5000/api/foodData",{
+      method: "POST",
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+
+    response = await response.json()
+    //console.log(response[1])
+
+    
+    setFoodItem(response[0])
+    setFoodCat(response[1])
+  }
+
+
+  useEffect(()=>{
+    loadData()
+  },[])
+
+
   return (
     <div>
 
@@ -16,11 +42,35 @@ const Home = () => {
         <Carousal />
       </div>
 
-      <div className="m-3">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="container">
+        {
+          foodCat !==[]
+          ?
+          foodCat.map((data)=>{
+              return (
+                <div>
+                  <div key={data._id} className="fs-3 m-3">
+                {data.CategoryName}
+                </div>
+                <hr />
+                {foodItem !==[]? foodItem.filter((item)=>item.CategoryName===data.CategoryName)
+                .map(filteredItems=>
+                  <div key={filteredItems._id}>
+                    <Card />
+                  </div>
+                )
+                :
+                 <div>No such data</div>}
+                </div>
+                
+                )
+          })
+          :
+          ""
+        }
+
+        
+        
       </div>
 
       <div>
