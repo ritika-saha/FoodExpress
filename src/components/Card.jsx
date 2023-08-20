3b0b664b9dcdad568301f4116cce2a5609929eb0
ritika-saha <ritika882001@gmail.com>
2023-08-21 03:26:57 +0530

    data display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+
+  let options=props.options
+  
+  let priceOptions=Object.keys(options)
+
   return (
     <div>
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-        <img src="https://th.bing.com/th/id/R.d9db32c03c737a34a9e7b12fd5304266?rik=jP1xpmPDgeYwqw&riu=http%3a%2f%2fimages.4ever.eu%2fdata%2fdownload%2faliments-et-boissons%2ftiramisu%2c-dessert-165263.jpg%3fno-logo&ehk=dmEZ1GSXU%2bzt6Q11nd2d71OVYc7HGXepH1wzBXN9fd4%3d&risl=&pid=ImgRaw&r=0" className="card-img-top" alt="..." />
+        <img src={props.imgSrc} className="card-img-top" alt="..." style={{height:"200px", objectFit:"fill"}} />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Lorem ipsum dolor sit amet.</p>
+          <h5 className="card-title text-warning fw-bolder">{props.foodName}</h5>
+          <p className="card-text mb-0">Order now! best quality in town</p>
           <div className="container w-100">
             <select className="m-2  h-100 bg-warning rounded">
               {Array.from(Array(6), (e, i) => {
@@ -18,12 +23,17 @@ const Card = () => {
                 );
               })}
             </select>
-            <select className="m-2  h-100 bg-warning rounded">
-              <option value="Half">Half</option>
-              <option value="Full">Full</option>
+            <select className="m-2 h-100 bg-warning rounded">
+              {
+                priceOptions.map((data)=>{
+                  return (
+                    <option key={data} value={data}>{data}</option>
+                  )
+                })
+              }
             </select>
 
-            <div className="d-inline h-100 fs-5">Total price</div>
+            <div className="h-100 fs-5">Total price- </div>
           </div>
         </div>
       </div>
