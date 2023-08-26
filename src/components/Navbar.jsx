a36f25b8017d0d92eef4aadc9891734cc2919415
ritika-saha <ritika882001@gmail.com>
2023-08-26 21:27:35 +0530

    login ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
+
 
 const Navbar = () => {
+  const navigate=useNavigate()
+  const handleLogout=()=>{
+    localStorage.removeItem("authToken")
+    navigate("/login")
+  }
   return (
     <div>
 
@@ -12,11 +18,31 @@ const Navbar = () => {
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <Link className="nav-link" aria-current="page" to='/'>Home</Link>
-        <Link className="nav-link" to='/login'>Login</Link>
-        <Link className="nav-link" to='/createuser'>SignUp</Link>
+      <div className="navbar-nav ms-auto d-flex">
+        <Link className="nav-link active fs-5" aria-current="page" to='/'>Home</Link>
+        {localStorage.getItem("authToken")?
+        <Link className="nav-link active fs-5" aria-current="page" to='/'>My Orders</Link>
         
+        :""
+      }
+        <div className='d-flex'>
+       {!localStorage.getItem("authToken")?
+       <>
+        <Link className="btn bg-white text-warning mb-2 mx-1" to='/login'>Login</Link>
+        <Link className="btn bg-white text-warning mb-2 mx-1" to='/createuser'>SignUp</Link>
+        </>
+        :
+        <>
+        <div className='btn bg-white text-warning mb-2 mx-1'>
+          🛒
+        </div>
+        <div className='btn bg-danger text-white mb-2 mx-1' onClick={handleLogout}>
+          Logout
+        </div>
+        </>
+        }
+        </div>
+
       </div>
     </div>
   </div>
