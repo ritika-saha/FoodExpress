dd8df2e9a5589cc80c8a22a1ef8cf36fa82f7f70
ritika-saha <ritika882001@gmail.com>
2023-08-30 13:49:42 +0530

    add to cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,18 @@ import { useDispatchCart, useCart } from "./ContextReducer";
 
 const Card = (props) => {
 
+  let dispatch=useDispatchCart()
+  let data=useCart()
   let options=props.options
   let priceOptions=Object.keys(options)
   const [qty, setQty]=useState(1)
   const [size, setSize]=useState("")
 
-  const handleAddToCart=()=>{
-    
+  const handleAddToCart=async()=>{
+    await dispatch({type:"ADD",id:props.foodItem._id,name: props.foodItem.name,price: props.finalPrice, qty:qty,size:size})
+    console.log(data)
   }
+  let finalPrice=qty * parseInt(options[size]);
   return (
     <div>
       <div className="card mt-3" style={{ width: "19rem", maxHeight: "460px" }}>
@@ -18,7 +22,7 @@ const Card = (props) => {
         <div className="card-body">
           <h5 className="card-title text-warning fw-bolder">{props.foodItem.name}</h5>
           <div className="container w-100">
-            <select className="m-2  h-100 bg-warning rounded">
+            <select className="m-2  h-100 bg-warning rounded" onChange={(e)=>setQty(e.target.value)}>
               {Array.from(Array(6), (e, i) => {
                 return (
                   <option key={i + 1} value={i + 1}>
@@ -27,7 +31,7 @@ const Card = (props) => {
                 );
               })}
             </select>
-            <select className="m-2 h-100 bg-warning rounded">
+            <select className="m-2 h-100 bg-warning rounded" onChange={(e)=>setSize(e.target.value)}>
               {
                 priceOptions.map((data)=>{
                   return (
@@ -37,7 +41,7 @@ const Card = (props) => {
               }
             </select>
 
-            <div className="h-100 fs-5">Total price- </div>
+            <div className="h-100 fs-5">Price = Rs.{finalPrice}/-</div>
             <hr />
             <button className="btn btn-warning justify-center ms-2 text-white fw-bold" onClick={handleAddToCart}>Add to Cart </button>
             
--- src/components/ContextReducer.jsx
+++ src/components/ContextReducer.jsx
@@ -4,7 +4,13 @@ const CartStateContext= createContext()
 const CartDispatchContext=createContext()
 
 const reducer=(state,action)=>{
-switch(action.type)
+switch(action.type){
+    case "ADD":
+        return [...state,{id:action.id,name:action.name,qty:action.qty,size:action.size,price:action.price,img:action.img}]
+
+        default:
+            console.log("error in reducer")
+}
 }
 
 export const CartProvider=({children})=>{
