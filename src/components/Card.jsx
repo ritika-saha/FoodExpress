4254c935e2c374d32dbba2d59d1b9ca8d430999c
ritika-saha <ritika882001@gmail.com>
2023-09-05 04:31:38 +0530

    cart update func

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,8 +13,30 @@ const Card = (props) => {
   const [size, setSize]=useState("")
 
   const handleAddToCart=async()=>{
+    let food=[]
+    for(const item of data){
+      if(item.id===props.foodItem._id){
+        food=item
+        break
+      }
+    }
+
+    if(food!==[]){
+      if(food.size===size){
+        await dispatch({type:"UPDATE",id:props.foodItem._id, price:finalPrice,qty:qty})
+        return
+      }
+    
+
+    else if(food.size!==size){
     await dispatch({type:"ADD",id:props.foodItem._id,name: props.foodItem.name,price: finalPrice, qty:qty,size:size})
-    console.log(data)
+   return  
+  }
+    // console.log(data)
+    return 
+  }
+    await dispatch({type:"ADD",id:props.foodItem._id,name: props.foodItem.name,price: finalPrice, qty:qty,size:size})
+
   }
   let finalPrice=qty * parseInt(options[size]);
   useEffect(()=>{
